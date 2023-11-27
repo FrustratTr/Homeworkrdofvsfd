39dffb09f04eaba0686b797836bf96a74cc3644f
Mark <mark.bogachev.00@mail.ru>
2023-11-27 19:37:13 +0300

    Fail to test added

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnPostedData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi everyone")
+                .contentType("text/plain; charset=UTF-8")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi everyon"));
+    }
+}
